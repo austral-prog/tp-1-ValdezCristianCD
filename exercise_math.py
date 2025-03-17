00ec1f93321c188eb24ac1ae196a37e9b381409f
ValdezCristianCD <cvaldez2@mail.austral.edu.ar>
2025-03-17 11:21:26 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -14,15 +14,21 @@
 #1
 #8.142857142857142
 
-print("hola")
-
 num1 = int(input("Ingrese el primer numero: "))
 num2 = int(input("Ingrese el segundo numero: "))
 
-print(f"La suma entre {num1} y {num2} es : {num1 + num2}")
-print(f"La diferencia entre {num1} y {num2} es : {num1 - num2}")
-print(f"El producto entre {num1} y {num2} es : {num1 * num2}")
-print(f"El promedio entre {num1} y {num2} es : {(num1 + num2) / 2}")
-print(f"El cociente entero entre {num1} y {num2} es : {round(num1 / num2)}")
-print(f"El resto de la division entera entre {num1} y {num2} es : {num1 % num2}")
-print(f"La division entre {num1} y {num2} es : {num1 / num2}")
+#print(f"La suma entre {num1} y {num2} es : {num1 + num2}")
+#print(f"La diferencia entre {num1} y {num2} es : {num1 - num2}")
+#print(f"El producto entre {num1} y {num2} es : {num1 * num2}")
+#print(f"El promedio entre {num1} y {num2} es : {(num1 + num2) / 2}")
+#print(f"El cociente entero entre {num1} y {num2} es : {round(num1 / num2)}")
+#print(f"El resto de la division entera entre {num1} y {num2} es : {num1 % num2}")
+#print(f"La division entre {num1} y {num2} es : {num1 / num2}")
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print((num1 + num2) / 2)
+print(round(num1 / num2))
+print(num1 % num2)
+print(num1 / num2)
